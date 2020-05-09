68dfd25bfe73c56b2cfbd1e8b0115eecb1b42340
aakashverma1124 <info.aakash11@gmail.com>
2020-05-09 12:01:33 +0530

    Framing is done

--- main.py
+++ main.py
@@ -1,1 +1,78 @@
-print("Hello")
+# importing tkinter library and all its classes
+
+from tkinter import *
+from tkinter.messagebox import *
+
+# utility font settings
+
+font = ('Verdana', 22, 'bold')
+
+# utility functions
+
+
+# creating a window
+
+window = Tk()
+window.title("Calculator")
+window.geometry('450x600')
+
+# adding a utility picture to the window
+pic = PhotoImage(file="img/technology.png")
+headingLabel = Label(window, image=pic)
+headingLabel.pack(side=TOP, pady=20)
+
+
+# heading Label
+heading = Label(window, text = "My Calculator", font=font, underline=0)
+heading.pack(side=TOP, pady=10)
+
+# textfield
+
+textField = Entry(window, font = font, justify=CENTER)
+textField.pack(side=TOP, pady = 20, fill=X, padx = 20)
+
+
+# buttons
+frame = Frame(window)
+frame.pack(side=TOP)
+
+
+# adding buttons now
+# button1 = Button(frame, text="+", font=font)
+# button1.pack(side=TOP)
+btnValue = 1
+for i in range(3):
+    for j in range(3):
+        button = Button(frame, text = btnValue, font=font, width = 5, height=2, relief='sunken', activebackground='red', activeforeground='white')
+        button.grid(row=i, column=j, padx=3, pady=3)
+        btnValue+=1
+
+dotbutton = Button(frame, text = ".", font=font, width = 5, height=2, relief='sunken')
+dotbutton.grid(row=3, column=0, padx=3, pady=3)
+
+zerobutton = Button(frame, text = "0", font=font, width = 5, height=2, relief='sunken')
+zerobutton.grid(row=3, column=1, padx=3, pady=3)
+
+equalbutton = Button(frame, text = "=", font=font, width = 5, height=2, relief='sunken')
+equalbutton.grid(row=3, column=2, padx=3, pady=3)
+
+plusButton = Button(frame, text = "+", font=font, width = 5, height=2, relief='sunken')
+plusButton.grid(row=0, column=3, padx=3, pady=3)
+
+minusButton = Button(frame, text = "-", font=font, width = 5, height=2, relief='sunken')
+minusButton.grid(row=1, column=3, padx=3, pady=3)
+
+multiplyButton = Button(frame, text = "*", font=font, width = 5, height=2, relief='sunken')
+multiplyButton.grid(row=2, column=3, padx=3, pady=3)
+
+divideButton = Button(frame, text = "/", font=font, width = 5, height=2, relief='sunken')
+divideButton.grid(row=3, column=3, padx=3, pady=3)
+
+clearButton = Button(frame, text = "C", font=font, width = 11, height=2, relief='sunken')
+clearButton.grid(row= 4, column=0, columnspan=2, padx=3, pady=3)
+
+allClearButton = Button(frame, text = "AC", font=font, width = 11, height=2, relief='sunken')
+allClearButton.grid(row=4, column=2, columnspan=2, padx=3, pady=3)
+
+
+window.mainloop()
